a1cfddbe644f5b9c5d23370ceb5d29dea2d0fa57
Esref Ozturk <esrefozturk93@gmail.com>
2014-07-09 12:37:16 +0300

    multiple sessions added

--- src/main/java/ManageEmployee.java
+++ src/main/java/ManageEmployee.java
@@ -12,12 +12,13 @@ import org.hibernate.cfg.Configuration;
 
 public class ManageEmployee {
     private static SessionFactory factory;
-    private static Session session;
-    private static Transaction tx;
+    private static Session session1,session2,currentSession;
+    private static Transaction tx1,tx2,currentTx;
     private static Scanner reader;
     private static String command;
+    private static int current;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         try{
             factory = new Configuration().configure().buildSessionFactory();
         }catch (Throwable ex) {
@@ -26,14 +27,21 @@ public class ManageEmployee {
         }
 
         reader = new Scanner(System.in);
-        session = factory.openSession();
-        tx = session.beginTransaction();
+        session1 = factory.openSession();
+        tx1 = session1.beginTransaction();
+        session2 = factory.openSession();
+        tx2 = session2.beginTransaction();
+        currentSession = session1;
+        currentTx = tx1;
+        current = 1;
+
 
         for(;;){
-            System.out.print("command: ");
+            Thread.sleep(100);
+            System.out.print( "["+ current +". session]command: ");
             command = reader.nextLine();
             if( command.equals("list") ){
-                List employees = session.createQuery("FROM Employee").list();
+                List employees = currentSession.createQuery("FROM Employee").list();
                 for (Iterator iterator =
                              employees.iterator(); iterator.hasNext();){
                     Employee employee = (Employee) iterator.next();
@@ -54,24 +62,34 @@ public class ManageEmployee {
                 System.out.print("Salary: ");
                 int salary = reader.nextInt();
                 reader.nextLine();
-                Employee employee = new Employee(id,fname, lname, salary);
-                int employeeID = (Integer) session.save(employee);
-                System.out.println("EmployeeID: "+employeeID);
+                Employee employee = new Employee(id, fname, lname, salary);
+                currentSession.save(employee);
             }
             else if( command.equals("delete") ){
                 System.out.print("EmployeeID: ");
                 int employeeId = reader.nextInt();
                 reader.nextLine();
-                Employee employee = (Employee)session.get(Employee.class, employeeId);
-                session.delete(employee);
+                Employee employee;
+                employee = (Employee) currentSession.get(Employee.class, employeeId);
+                currentSession.delete(employee);
             }
             else if( command.equals("close") ){
-                tx.commit();
-                session.close();
+                currentTx.commit();
+                currentSession.close();
             }
             else if( command.equals("open") ){
-                session = factory.openSession();
-                tx = session.beginTransaction();
+                if( current==1 ){
+                    session1 = factory.openSession();
+                    tx1 = session1.beginTransaction();
+                    currentSession = session1;
+                    currentTx = tx1;
+                }
+                else{
+                    session2 = factory.openSession();
+                    tx2 = session2.beginTransaction();
+                    currentSession = session2;
+                    currentTx = tx2;
+                }
             }
             else if( command.equals("help") ){
                 System.out.println("help         this menu");
@@ -80,13 +98,26 @@ public class ManageEmployee {
                 System.out.println("delete       delete and employee");
                 System.out.println("open         open session and begin transaction");
                 System.out.println("close        commit transaction and close session");
+                System.out.println("change       change between two sessions");
 
             }
             else if( command.equals("quit") ) {
-                tx.commit();
-                session.close();
+                currentTx.commit();
+                currentSession.close();
                 factory.close();
                 break;
+            }
+            else if( command.equals("change") ){
+                if( currentSession.equals( session1 ) ) {
+                    currentSession = session2;
+                    currentTx = tx2;
+                    current = 2;
+                }
+                else {
+                    currentSession = session1;
+                    currentTx = tx1;
+                    current = 1;
+                }
             }
             else{
                 System.out.println("command not found. Use help menu");
